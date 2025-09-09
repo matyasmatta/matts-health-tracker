8fe0ebe3b0535378eac2acd6444f8643cebc168e
Matyáš Matta <matyasmatta@email.cz>
2025-09-10 00:00:10 +0200

    version 1.11.2 - made MedicationScreen.kt customisable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 33
         targetSdk = 34
         versionCode = 1
-        versionName = "1.10.17"
+        versionName = "1.11.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
